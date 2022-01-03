d3b7e19b39b5e00b6af13ffbe5721eff62892a05
sandeep singh <sandy.singh51480@gmail.com>
2022-01-03 09:03:38 +0000

    install excel package

--- config/app.php
+++ config/app.php
@@ -174,7 +174,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        Maatwebsite\Excel\ExcelServiceProvider::class,
     ],
 
     /*
@@ -228,7 +228,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'ChatStatus' => App\Helpers\StatusChat::class,
         'MailHelper' => App\Helpers\MailHelper::class,
-
+	'Excel' => Maatwebsite\Excel\Facades\Excel::class,
     ],
 
 ];
